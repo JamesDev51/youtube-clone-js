d681165e5c961584220a85d9d80c62accc813a20
james <minseok.jeong.james@gmail.com>
2021-01-09 23:57:36 +0900

    please

--- src/init.js
+++ src/init.js
@@ -1,9 +1,10 @@
 
 import app from "./app"
 import "./db"
+import dotenv from "dotenv"
+dotenv.config();
 
-
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => 
     console.log(`✅ Listening on: http://localhost:${PORT}`);
